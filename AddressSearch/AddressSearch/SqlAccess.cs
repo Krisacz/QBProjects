1de6c1b4b7cc4c3f7aacf624cce9bc59c962e301
KS <krisacz@yahoo.com>
2016-04-28 09:38:29 +0100

    AddressSearch initial commit.

---
+++ AddressSearch/AddressSearch/SqlAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AddressSearch
+{
+    public class SqlAccess
+    {   
+        public static string GetFullAddress(string number, string postcode)
+        {
+            var result = string.Empty;
+            try
+            {
+                using (var conn = new SqlConnection(@"Server=192.168.2.111,1433\miwh; DataBase=RoyalMailPostCodes; User Id=kssql; Password=Kena654654;"))
+                //using (var conn = new SqlConnection(@"Server=192.168.2.111,1433\miwh; DataBase=RoyalMailPostCodes; Trusted_Connection=True;"))
+                {
+                    conn.Open();
+                    var cmd = new SqlCommand("GetFullAddress", conn) { CommandType = CommandType.StoredProcedure };
+                    cmd.Parameters.Add(new SqlParameter("@Number", number));
+                    cmd.Parameters.Add(new SqlParameter("@Postcode", postcode));
+
+                    using (var rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            result = rdr["Result"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result = string.Format("ERROR: {0}", ex.Message);
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ AddressSearch/WsTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WsTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ws = new AddressSearchWS.WebService1SoapClient();
+            var result = ws.GetFullAddress("89", "SK14 6JQ");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
