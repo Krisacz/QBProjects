2b006cf7974ab642d2038409cfcbd7d200859e02
KS <krisacz@yahoo.com>
2016-07-15 14:25:47 +0100

    Added DobFix to CustomFixer.cs

--- LeadsImporter/LeadsImporter.Lib/Validation/CustomFixer.cs
+++ LeadsImporter/LeadsImporter.Lib/Validation/CustomFixer.cs
@@ -8,9 +8,34 @@ namespace LeadsImporter.Lib.Validation
         public static IEnumerable<string> Fix(List<string> dataRow, string type)
         {
             var afterFix1 = DateExecutedFix(dataRow, type);
+            var afterFix2 = DobFix(afterFix1 ?? dataRow, type);
             //any other fixes if needed e.g. var afterFix2 = FixSomething(afterFix1, type);
             //....
-            return afterFix1 ?? dataRow;
+            return afterFix2 ?? dataRow;
+        }
+
+        private static List<string> DobFix(IEnumerable<string> dataRow, string type)
+        {
+            var fixedDataRow = new List<string>(dataRow);
+            int index;
+            switch (type.ToLower())
+            {
+                case "ursc": index = 4; break;
+                case "rppi": index = 4; break;
+                default: return null;
+            }
+            if (fixedDataRow.Count < index) return null;
+            var oldValue = fixedDataRow[index];
+            if (oldValue.Length < 10) return null;
+
+            var d = oldValue.Substring(8, 2);
+            var m = oldValue.Substring(5, 2);
+            var y = oldValue.Substring(0, 4);
+
+            var newValue = $"{d}/{m}/{y}";
+            fixedDataRow[index] = newValue;
+
+            return fixedDataRow;
         }
 
         private static List<string> DateExecutedFix(IEnumerable<string> dataRow, string type)
